a5d2fd24661da264a0b3675c45ccf633bf4f09d5
Mirna Pibernik <mpiberni@gmail.com>
2022-02-11 22:17:00 +0100

    Cleanup for deployment

--- server/db/db.ts
+++ server/db/db.ts
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 
 export default {
   db: async () => {
-    const client = await new MongoClient(process.env.DB_URL!).connect();
-    return client.db(process.env.DB_NAME!);
+    const client = await new MongoClient(process.env.DATABASE_URL!).connect();
+    return client.db(process.env.DATABASE_NAME!);
   },
 };
